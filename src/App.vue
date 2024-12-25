980674b9f12bb8b73f029f2a98f43f4405ab8177
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-25 23:32:44 +0300

    feat: store to refs

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,30 @@
     <div class="container">
       <div class="new-diary">
         <new-journal></new-journal>
-
       </div>
 
       <div class="data-area">
-        <p v-if="filter === 'all'">There are a total of {{ journalStore.journalLength }} records</p>
-        <p v-if="filter === 'favs'">There are a total of {{ journalStore.journalFavCount }} records</p>
+        <p v-if="filter === 'all'">There are a total of {{ journalLength }} records</p>
+        <p v-if="filter === 'favs'">There are a total of {{ journalFavCount }} records</p>
         <div>
-          <button @click="filter = 'all'" :class="{active: filter === 'all'}">All Journal</button>
-          <button @click="filter = 'favs'" :class="{active: filter === 'favs'}">Favorite Journal</button>
+          <button @click="filter = 'all'" :class="{ active: filter === 'all' }">All Journal</button>
+          <button @click="filter = 'favs'" :class="{ active: filter === 'favs' }">Favorite Journal</button>
         </div>
       </div>
       <div class="journal-list">
-        <template v-for="journal in journalStore.journal" v-if="filter === 'all'">
+        <template v-for="journal in journal" v-if="filter === 'all'">
           <journal-detail :journal="journal"></journal-detail>
         </template>
-        <template v-for="journal in journalStore.favs" v-if="filter === 'favs'">
+        <template v-for="journal in favs" v-if="filter === 'favs'">
           <journal-detail :journal="journal"></journal-detail>
         </template>
       </div>
     </div>
   </main>
+
+  <div class="reset-data">
+    <button @click="journalStore.$reset">Reset Data</button>
+  </div>
 
 </template>
 
@@ -40,6 +43,7 @@ import { defineComponent, ref } from 'vue'
 import { useJournalStore, type Journal } from './stores/JournalStore';
 import JournalDetails from './components/journal-details.vue';
 import NewJournal from './components/new-journal.vue';
+import { storeToRefs } from 'pinia';
 
 
 export default defineComponent({
@@ -50,8 +54,11 @@ export default defineComponent({
   setup() {
     const journalStore = useJournalStore();
     const filter = ref("all");
+
+    const { journal, loading, favs, journalFavCount, journalLength} = storeToRefs(journalStore);
+
     journalStore.getJournal();
-    return { journalStore, filter };
+    return { journalStore, filter, journal, loading, favs, journalFavCount, journalLength};
   }
 
 })
